bc0d91643171618d6be3f2e4560d87d0c472d198
muhammadazeemkhan <121564168+muhammadazeemkhan@users.noreply.github.com>
2023-12-08 23:18:20 +0500

    Added Cart.HTML

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ import {
   ref,
   set,
   push,
+  onValue,
+  child,
+  get,
+  orderByChild,
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 // import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-storage.js";
 
@@ -40,10 +44,12 @@ const database = getDatabase(app);
 onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
-    document.getElementById("spinner").style.display = "none";
+    // document.getElementById("spinner").style.display = "none";
 
     const uid = user.uid;
     console.log(uid);
+
+    getCartItems();
 
     // ...
   } else {
@@ -314,6 +320,52 @@ const addToCart = async (btn, id) => {
     });
   }
 };
+
+function getCartItems() {
+  const cartContainer = document.getElementById("cartContainer");
+  const cartRef = ref(database, "cart/" + auth.currentUser.uid);
+  onValue(cartRef, (snapshot) => {
+    const isDataExist = snapshot.exists();
+    if (isDataExist) {
+      // cartContainer.innerHTML = null;
+      snapshot.forEach((childSnapShot) => {
+        const childKey = childSnapShot.key;
+        const childData = childSnapShot.val();
+        const { id, thumbnail, title, price, category } = childData;
+
+        const card = `
+        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" data-aos="fade-right" >
+      <a href="#">
+          <img src=${thumbnail}
+                  alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+          <div class="px-4 py-3 w-72">
+              <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+              <p class="text-lg font-bold text-black truncate block capitalize">${title}</p>
+              <div class="flex items-center">
+                  <p class="text-lg font-semibold text-black cursor-auto my-3">$ ${price}</p>
+                  <del>
+                      <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                  </del>
+                  <div class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                          fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd"
+                              d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                          <path
+                              d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                      </svg></div>
+              </div>
+          </div>
+      </a>
+  </div>`;
+        // cartContainer.innerHTML += card;
+
+        console.log("childKey", childKey);
+        console.log("childData", childData);
+        console.log(thumbnail);
+      });
+    }
+  });
+}
 
 window.getPorducts = getPorducts;
 window.addToCart = addToCart;
