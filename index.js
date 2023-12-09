066a73f8567912a89d1d8feee4b347143a6a3c23
muhammadazeemkhan <121564168+muhammadazeemkhan@users.noreply.github.com>
2023-12-09 22:12:43 +0500

    Delete Functionality Added

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ import {
   onAuthStateChanged,
   signOut,
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-// import {
-//   getFirestore,
-//   addDoc,
-//   collection,
-// } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+
 import {
   getDatabase,
   ref,
@@ -22,8 +18,8 @@ import {
   child,
   get,
   orderByChild,
+  remove,
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
-// import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCikFNmz2CE7VDyr_QAghlW-94ezu_HXKM",
@@ -43,7 +39,6 @@ const database = getDatabase(app);
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    // User is signed in, see docs for a list of available properties
     // document.getElementById("spinner").style.display = "none";
 
     const uid = user.uid;
@@ -204,7 +199,7 @@ getPorducts();
 
 //FUNCTION ON CATEGOERY FUNCTION
 
-const remove = (btn) => {
+const removeCard = (btn) => {
   container.innerHTML = null;
   async function checking() {
     const productsResponse = await fetch("https://dummyjson.com/products").then(
@@ -301,9 +296,7 @@ async function getData() {
 //ADD TO CART FUNCTION
 
 const addToCart = async (btn, id) => {
-  // btn.preventDefault;
   if (auth.currentUser) {
-    // .then(async (json) => console.log(json));
     const cartRef = ref(database, "cart/" + auth.currentUser.uid);
     const newRef = push(cartRef);
     set(newRef, {
@@ -327,53 +320,102 @@ function getCartItems() {
   onValue(cartRef, (snapshot) => {
     const isDataExist = snapshot.exists();
     if (isDataExist) {
-      // cartContainer.innerHTML = null;
+      cartContainer.innerHTML = null;
       snapshot.forEach((childSnapShot) => {
         const childKey = childSnapShot.key;
         const childData = childSnapShot.val();
-        const { id, thumbnail, title, price, category } = childData;
+        const { id, thumbnail, title, price, category, description } =
+          childData;
 
         const card = `
-        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" data-aos="fade-right" >
-      <a href="#">
-          <img src=${thumbnail}
-                  alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
-          <div class="px-4 py-3 w-72">
-              <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
-              <p class="text-lg font-bold text-black truncate block capitalize">${title}</p>
-              <div class="flex items-center">
-                  <p class="text-lg font-semibold text-black cursor-auto my-3">$ ${price}</p>
-                  <del>
-                      <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                  </del>
-                  <div class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                          fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
-                          <path fill-rule="evenodd"
-                              d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
-                          <path
-                              d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                      </svg></div>
-              </div>
-          </div>
-      </a>
-  </div>`;
-        // cartContainer.innerHTML += card;
+        
+        <section class="container mx-auto p-10 md:p-20 antialiased ">
+    <article
+        class=" flex flex-wrap md:flex-nowrap shadow-lg mx-auto max-w-3xl group cursor-pointer transform duration-500 hover:-translate-y-1">
+        <img class="w-full max-h-[400px] object-fit md:w-52" src="${thumbnail}" alt="">
+        <div class="">
+            <div class="p-5 pb-10">
+                <h1 class="text-2xl font-semibold text-gray-800 mt-4">
+                    ${title}
+                </h1>
+                <p class="text-xl text-gray-400 mt-2 leading-relaxed">
+                    Located in Rajshahi Division, Bogra is one of the oldest and most fascinating towns in Bangladesh
+                </p>
+            </div>
+            <div class="bg-blue-50 p-5">
+                <div class="sm:flex sm:justify-between">
+                    <div>
+                        <div class="text-lg text-gray-700">
+                            <span class="text-gray-900 font-bold">Price : $ </span> ${price}
+                        </div>
+                        <div class="flex items-center">
+                            <div class="flex">
+                                <svg class="w-4 h-4 mx-px fill-current text-green-600"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+                                    <path
+                                        d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z">
+                                    </path>
+                                </svg>
+                                <svg class="w-4 h-4 mx-px fill-current text-green-600"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+                                    <path
+                                        d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z">
+                                    </path>
+                                </svg>
+                                <svg class="w-4 h-4 mx-px fill-current text-green-600"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+                                    <path
+                                        d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z">
+                                    </path>
+                                </svg>
+                                <svg class="w-4 h-4 mx-px fill-current text-green-600"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+                                    <path
+                                        d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z">
+                                    </path>
+                                </svg>
+                                <svg class="w-4 h-4 mx-px fill-current text-green-600"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+                                    <path
+                                        d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z">
+                                    </path>
+                                </svg>
+                            </div>
+                            <div class="text-gray-600 ml-2 text-sm md:text-base mt-1">
+                                16 reviews
+                            </div>
+                        </div>
+                    </div>
+                    <button onclick="deleteCart(this)" id="${childKey}" class="mt-3 sm:mt-0 py-2 px-5 md:py-3 md:px-6 bg-purple-700 hover:bg-purple-600 font-bold text-white md:text-lg rounded-lg shadow-md">
+              Delete
+            </button>
+                </div>
+                
+            </div>
+        </div>
+    </article>
+</section>`;
 
-        console.log("childKey", childKey);
-        console.log("childData", childData);
-        console.log(thumbnail);
+        cartContainer.innerHTML += card;
       });
     }
   });
 }
 
+function deleteCart(btn) {
+  const elemntID = btn.id;
+  const cartRef = ref(database, `cart/${auth.currentUser.uid}/${elemntID}`);
+  remove(cartRef);
+}
+
 window.getPorducts = getPorducts;
 window.addToCart = addToCart;
-window.remove = remove;
+window.removeCard = removeCard;
 window.getData = getData;
 window.userRegisteration = userRegisteration;
 window.logOut = logOut;
 window.logIn = logIn;
+window.deleteCart = deleteCart;
 
 // async function fakeApi(){
 //     const call = await fetch('https://reqres.in/api/users?page=2').then(res=>res.json())
